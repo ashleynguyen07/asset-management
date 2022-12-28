7b60f131fa167e6fc6e1d5b92e8639fa0b731dd7
Ashley Nguyen <nguyentramy.se@gmail.com>
2022-12-28 17:39:32 +0700

    check id & password EM

--- FE/login.js
+++ FE/login.js
@@ -6,39 +6,55 @@ function login() {
     let password = document.getElementById("password").value;
 
     fetch("http://localhost:8080/login", {
-     
-    // Adding method type
-    method: "POST",
-     
-    // Adding body or contents to send
-    body: JSON.stringify({
-        employeeID: employeeID,
-        password: password,
-    }),
-     
-    // Adding headers to the request
-    headers: {
-        "Content-type": "application/json; charset=UTF-8"
-    }
-}).then((data) => {
+
+        // Adding method type
+        method: "POST",
+
+        // Adding body or contents to send
+        body: JSON.stringify({
+            employeeID: employeeID,
+            password: password,
+        }),
+
+        // Adding headers to the request
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    }).then((data) => {
         console.log(data);
-        return data.json(); //converted to object
+        if (data === undefined) {
+            return data;
+        } else {
+            return data.json(); //converted to object
+        }
     }).then((objectData) => {
         console.log(objectData);
-        let tableData = "";
-        objectData.map((values) => {
-            tableData += `
-    <tr>
-        <td>${values.employID}</td>
-        <td>${values.name}</td>
-        <td>${values.birthdate}</td>
-        <td>${values.role}</td>
-        <td>${values.sex}</td>
-        <td>${values.password}</td>
-    </tr>`;
-        });
-        document.getElementById("table-body").innerHTML = tableData;
+        if (objectData === undefined) {
+            console.log("Login Fail!");
+        } else {
+            console.log(objectData);
+            alert("Login thanhf coong");
+            
+            window.location.href = "/FE/index.html"
+            let tableData = "";
+    //         objectData.map((values) => {
+    //             tableData += `
+    // <tr>
+    //     <td>${values.employID}</td>
+    //     <td>${values.name}</td>
+    //     <td>${values.birthdate}</td>
+    //     <td>${values.role}</td>
+    //     <td>${values.sex}</td>
+    //     <td>${values.password}</td>
+    // </tr>`;
+    //         });
+            // document.getElementById("table-body").innerHTML = tableData;
+        }
+    }).catch((e) => {
+        console.log(e);
     })
+
+    // history.push("/employees");
 
 }
 
